b747eaa560841a47af66746c5d2ff14a8017549e
Alan <holasoyalan9@gmail.com>
2022-05-07 00:12:19 -0500

    Empezamos a agregar la lógica para la petición de la api

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const pAdvice = document.getElementById("advicePh");
 const pPrincipal = document.getElementById("text-advice")
 const button = document.getElementById("dado");
+const api = "https://api.adviceslip.com/advice/"
+const fetchData  = require("./utils/fetchData");
+
 const logic = () =>{
-  number = 177
-  pPrincipal.innerText = "Funcionaa!!";
+  const getElement = async (url_api) =>{
+    try{
+      const allElement = await fetchData(url_api);
+      return allElement;
+    }catch(error){
+      console.log(error)
+    }
+  }
+  getElement(api);
+  pPrincipal.innerText = `${allElement}`;
   pAdvice.innerText = `Advice #${number}`
 }
 
